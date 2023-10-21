715e69cfe0230d92c56e255b4d53f81e74cddd54
Alexeykaa <ka.alexey@gmail.com>
2023-10-21 19:40:19 +0300

    init implementation

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,74 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
+
+    private static final int SIGNATURE_WITHOUT_ACCESS_MODIFIER_LENGTH = 2;
+    private static final int SIGNATURE_WITH_ACCESS_MODIFIER_LENGTH = 3;
+    private static final int ARGUMENT_PARTS_LENGTH = 2;
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        // split into two parts - descriptions of the method and the arguments
+        String[] token = signatureString.split("[\\(\\)]");
+        String argumentsPart = "";
+        if (token.length > 1) {
+            // argument exists
+            argumentsPart = token[1].trim();
+        }
+        List<MethodSignature.Argument> arguments = parseArgument(argumentsPart);
+        MethodSignature result = parseMethod(token[0], arguments);
+        return result;
+    }
+
+    private List<MethodSignature.Argument> parseArgument(String description) {
+        List<MethodSignature.Argument> result = new ArrayList<>();
+        if (description.length() > 0) {
+            String[] token = description.trim().split(",");
+            for (String argument : token) {
+                String[] parts = argument.trim().split(" ");
+                if (parts.length != ARGUMENT_PARTS_LENGTH) {
+                    throw new IllegalArgumentException("Invalid the argument description: " + argument);
+                }
+                result.add(new MethodSignature.Argument(parts[0], parts[1]));
+            }
+        }
+        return result;
+    }
+
+    private MethodSignature parseMethod(String description, List<MethodSignature.Argument> arguments) {
+        String[] token = description.split(" ");
+        if (token.length < SIGNATURE_WITHOUT_ACCESS_MODIFIER_LENGTH
+                || token.length > SIGNATURE_WITH_ACCESS_MODIFIER_LENGTH) {
+            throw new IllegalArgumentException("Invalid the method signature: " + description);
+        }
+        String methodName = token[token.length - 1];
+        MethodSignature result = new MethodSignature(methodName, arguments);
+        String returnType = token[token.length - 2];
+        result.setReturnType(returnType);
+        if (token.length == SIGNATURE_WITH_ACCESS_MODIFIER_LENGTH) {
+            // signature with access modifier
+            String modifier = token[0];
+            result.setAccessModifier(modifier);
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,24 @@
 package com.epam.mjc;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StringSplitter {
+
+    private static final String REGEXP_DELIMITER_PATTERN = "([%s])+";
 
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String delimiter = String.format(REGEXP_DELIMITER_PATTERN, String.join("", delimiters));
+        String[] result = source.split(delimiter);
+        return Arrays.stream(result).filter(s -> !s.isEmpty()).collect(Collectors.toList());
     }
 }
